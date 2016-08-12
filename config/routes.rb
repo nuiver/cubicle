1863fcd1dd4654fdd8879a59321a738f12d6fb1a
Mark Nuiver <mark@vrijblikken.nl>
2016-08-12 22:00:20 +0200

    not working profile added to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+
+  resources :users, only: :index
+  resource :profile, only: [:show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
