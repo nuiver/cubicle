488a5ebb4e6bc86373dcae05c68bdc58050b1b08
Mark Nuiver <mark@vrijblikken.nl>
2016-08-21 22:27:35 +0200

    Config yml cloudinary and uploaders

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dqev3rwfk'
-  config.api_key = '919144445152722'
-  config.api_secret = 'z13OzYwnDXeF4liVt6dmcpteYVY'
-  config.cdn_subdomain = true
-end
