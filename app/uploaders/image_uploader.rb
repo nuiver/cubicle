2736239c45b1bc9ff5e5b7e90c9374db8d205db4
Mark Nuiver <mark@vrijblikken.nl>
2016-08-22 07:33:13 +0200

    Comment out store_dir

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -62,7 +62,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
 
-  # Override the filename of the uploaded files:
+  # Override the filename of the uploaded files:h
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
