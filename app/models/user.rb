53c463ed57c9d1929f5ceaa94c71b3f42b75c56e
Mark Nuiver <mark@vrijblikken.nl>
2016-08-31 19:14:34 +0200

    One step sign in complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,16 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  before_create :set_default_role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  private
+    def set_default_role
+    self.role_id ||= Role.find_by_name('Member')
+  end
 
 end
