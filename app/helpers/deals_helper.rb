6e613ee0a539fb911990739c868135673c1e2930
Mark Nuiver <mark@vrijblikken.nl>
2016-09-15 12:50:03 +0200

    Business logic for date input jQUery - in progress

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,2 +1,18 @@
 module DealsHelper
+
+  def get_range(piece)
+    ranges = []
+    if piece.deals.any?
+      piece.deals.sort.each_with_index do | deal, i |
+        ranges << {("beg") => deal.begin_res.strftime("%Y-%B-%d"), ("end") => deal.end_res.strftime("%Y-%B-%d")}
+      end
+    end
+
+    ranges.to_json.html_safe
+  end
+
+  def get_range_length(piece)
+    piece.deals.length.to_s
+  end
+
 end
